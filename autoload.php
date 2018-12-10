66d5f33fa126305b97e04f7d0843bf7f3f0b411d
Henrique Oliveira <henrique.oliveira@ampla.com.br>
2018-12-10 14:50:27 -0300

    Simplificando condição de checagem do arquivo físico.

--- autoload.php
+++ autoload.php
@@ -5,10 +5,8 @@ function __autoload($classe){
     $diretorioBase="classes";
     $interator=new RecursiveIteratorIterator(new RecursiveDirectoryIterator($diretorioBase));
     foreach ($interator as $elemento){
-        if(strpos($elemento,$classe)!==FALSE){
-            if(file_exists($elemento)){
-                include_once $elemento;	
-            }
+        if(strpos($elemento,$classe)!==FALSE && file_exists($elemento)){
+            include_once $elemento;	
         }
     }
     unset($elemento,$interator,$diretorioBase);
